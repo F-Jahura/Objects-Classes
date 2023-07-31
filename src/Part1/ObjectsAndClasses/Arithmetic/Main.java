c92f8db70c1906f0e641d158b1daa84175b65e8b
F-Jahura <f_jahura@yahoo.com>
2023-07-31 15:51:17 +0300

    main of task1

---
+++ src/Part1/ObjectsAndClasses/Arithmetic/Main.java
@@ -0,0 +1,12 @@
+package Part1.ObjectsAndClasses.Arithmetic;
+
+public class Main {
+    public static void main(String[] args) {
+        Arithmetic arithmetic = new Arithmetic(20, 10);
+
+        arithmetic.sumNum();
+        arithmetic.multiplyNum();
+        arithmetic.maxNum();
+        arithmetic.minNum();
+    }
+}
